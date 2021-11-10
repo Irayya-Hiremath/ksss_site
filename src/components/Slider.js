b40c9ab792d40281e7016ec7c49435a7ea9d0975
irayya <ihiremath1991@gmail.com>
2021-11-10 12:56:32 +0530

    new pages updated

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container,Carousel} from 'react-bootstrap';
-import image1 from "../images/1.jpg"
-import image2 from "../images/2.jpg"
-import image3 from "../images/3.jpg"
-import image4 from "../images/4.jpg"
-import image5 from "../images/5.jpg"
-import image6 from "../images/6.jpg"
-import image7 from "../images/7.jpg"
+import image1 from "../images/1.jpg";
+import image2 from "../images/2.jpg";
+import image3 from "../images/3.jpg";
+import image4 from "../images/4.jpg";
+import image5 from "../images/5.jpg";
+import image6 from "../images/6.jpg";
+import image7 from "../images/7.jpg";
 
 function Slider(){
     return(
