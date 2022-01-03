f1a59fbeba1069d1d81c99f104d5f6b1503c5aaf
irayya <ihiremath1991@gmail.com>
2022-01-03 12:46:26 +0530

    checking font size

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,5 +1,5 @@
   
-const Textcontent=[
+ const Textcontent=[
 
     {
         Title:'WELCOME TO K.S.S.S P.U COLLEGE',
@@ -19,104 +19,38 @@ const Textcontent=[
         round development of the individual student is the foremost
         philosophy of this college.The campus is optly named as "Govardan
         Campus`
-    }
-    // {
-    //     Title:'THE DRIVING FORCE',
-    //     Subtitle:'',
-    //     Text:`We believe that every child is uniquely created with divine
-    //     energy which requires a suitable medium of expression. It is our
-    //     duty to channelize this hidden energy to the world at large
-    //     through right education, right values and right action. KSSS PU
-    //     College imbibes these ideals for purposeful education across
-    //     caste, creed and nationality in the true spirit of Sri Swamiji's
-    //     vision.`
-    // },
-    // {
-    //     Title:'Objective',
-    //     Subtitle:'',
-    //     Text:`We aim to provide quality education with highest academic
-    //     standards .Our objective is to create an environment that will
-    //     ensure that our student will develop well rounded personalities
-    //     and grow into successful human beings and not just remain
-    //     successful students.`
-    // },
-    // {
-    //     Title:'Vision',
-    //     Subtitle:'',
-    //     Text:`To maintain the higher standard of discipline and imparting right
-    //     value. To install the spirit of dedication to the young minds to
-    //     be an asset to the nation. To mould the student/young minds with
-    //     best education and higher disciplinary Standards to meet the
-    //     global competency. To be the most efficient acceptable and excel
-    //     in the field of education`
-    // },
+    },
+    {
+        Title:'THE DRIVING FORCE',
+        Subtitle:'',
+        Text:`We believe that every child is uniquely created with divine
+        energy which requires a suitable medium of expression. It is our
+        duty to channelize this hidden energy to the world at large
+        through right education, right values and right action. KSSS PU
+        College imbibes these ideals for purposeful education across
+        caste, creed and nationality in the true spirit of Sri Swamiji's
+        vision.`
+    },
+    {
+        Title:'Objective',
+        Subtitle:'',
+        Text:`We aim to provide quality education with highest academic
+        standards .Our objective is to create an environment that will
+        ensure that our student will develop well rounded personalities
+        and grow into successful human beings and not just remain
+        successful students.`
+    },
+    {
+        Title:'Vision',
+        Subtitle:'',
+        Text:`To maintain the higher standard of discipline and imparting right
+        value. To install the spirit of dedication to the young minds to
+        be an asset to the nation. To mould the student/young minds with
+        best education and higher disciplinary Standards to meet the
+        global competency. To be the most efficient acceptable and excel
+        in the field of education`
+    },
 ]
 
 
-
-// import React from 'react'
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import {Container,Carousel} from 'react-bootstrap';
-// import image1 from "../images/1.jpg"
-// import image2 from "../images/2.jpg"
-// import image3 from "../images/3.jpg"
-// import image4 from "../images/4.jpg"
-// import image5 from "../images/5.jpg"
-// import image6 from "../images/6.jpg"
-// import image7 from "../images/7.jpg"
-
-// function Data(){
-//     return(
-//         <div>
-      
-//         <Container fluid className='border border-danger'>
-  
-//           {/*********  third row *************/}
-        
-//           <Carousel>
-//               <Carousel.Item style={{height:'80vh',background:'no-repeat scroll center  scroll',backgroundSize:'cover'}}>
-//               <div
-//                     className="d-block w-100"
-//                     style={{backgroundImage: `url(${image1})`,    height:'100%',
-//                     backgroundPosition: 'center',
-//                     backgroundRepeat: 'no-repeat',
-//                     backgroundSize: 'cover'}}
-                    
-//                     ></div>
-//               </Carousel.Item>
-  
-//               <Carousel.Item style={{height:'80vh'}}>
-//               <div
-//                     className="d-block w-100"
-//                     style={{backgroundImage: `url(${image7})`,    height:'100%',
-//                     backgroundPosition: 'center',
-//                     backgroundRepeat: 'no-repeat',
-//                     backgroundSize: 'cover'}}
-                    
-//                     ></div>
-                  
-//               </Carousel.Item>
-  
-//               <Carousel.Item style={{height:'80vh'}}>
-//               <div
-//                     className="d-block w-100"
-//                     style={{backgroundImage: `url(${image4})`,    height:'100%',
-//                     backgroundPosition: 'center',
-//                     backgroundRepeat: 'no-repeat',
-//                     backgroundSize: 'cover'}}
-                    
-//                     ></div>
-//               </Carousel.Item>
-  
-            
-  
-//           </Carousel>
-  
-  
-//           </Container>
-  
-//       </div>
-
-//     )
-// }
-// export default Data
+export default Textcontent
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,14 +4,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Image from '../components/Image'
 import Footer from '../components/Footer'
 import Content from '../components/Content';
+import Textcontent from '../components/Data'
+
 
 
 function Contact() {
+    console.log(Textcontent)
     return (
         <div className='overflow-hidden'>
              <Layout>
 
-            <Content title="jaaai" titleContent='testing'/>
+            <Content title={Textcontent[0].Title} titleContent='testing'/>
             <div className="row">
                 <div className="col">
             <Content title="jaaai" titleContent="testig props"/>
